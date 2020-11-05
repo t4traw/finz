d5a5244f67286706a39e673a0e6bd4f748c0bb6f
TatsuroMoriyama <t4traw@gmail.com>
2020-11-05 11:17:43 +0900

    mailの設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,16 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'finz.me'}
+  config.action_mailer.default_url_options = { host: 'https://www.finz.me' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
