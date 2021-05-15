edf941ecf0f3c91f354a8c10d8e123b907916177
t4traw <t4traw@gmail.com>
2021-05-15 11:45:47 +0900

    メール送信をSendgridからMailgunに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: host,
+    authentication: :plain
+}
 
   # Settings specified here will take precedence over those in config/application.rb.
 
