e78b7c3dc4210ea387b880d16259f518f354e9d1
t4traw <t4traw@gmail.com>
2021-08-16 19:28:36 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+t4traw = User.create(
+  name: "t4traw",
+  email: "t4traw@gmail.com",
+  password: "password",
+  profile: "アングラーのタックルボックスなど、釣具や装備写真を共有するサービス https://finz.me を運営しています🎣",
+  agreement: true,
+)
+t4traw.activate!
+
+rand(1..3).times do
+  tackle = t4traw.tackles.create(
+    description: Faker::Games::Dota.quote,
+    photo: open("#{Rails.root}/db/seed_data/images/500/#{"#{Random.rand(1..24)}.jpg"}"),
+  )
+  rand(1..3).times do
+    tackle.tag_list.add(Faker::Games::Dota.hero)
+  end
+  tackle.save
+end
+
+10.times do
+  user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "password",
+    profile: Faker::Lorem.sentence,
+    agreement: true,
+  )
+  user.activate!
+
+  rand(1..3).times do
+    tackle = user.tackles.create(
+      description: Faker::Games::Dota.quote,
+      photo: open("#{Rails.root}/db/seed_data/images/500/#{"#{Random.rand(1..24)}.jpg"}"),
+    )
+    rand(1..3).times do
+      tackle.tag_list.add(Faker::Games::Dota.hero)
+    end
+    tackle.save
+  end
+end
